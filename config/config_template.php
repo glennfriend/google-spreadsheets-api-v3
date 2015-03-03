e2eb76ff5eab0fc993bbc820a062808b269415e9
turtlemt <turtlemt@gmail.com>
2015-03-03 13:57:36 +0800

    Add config file

---
+++ config/config_template.php
@@ -0,0 +1,12 @@
+<?php
+// timezone
+define('APPLICATION_TIMEZONE', 'Asia/Taipei' );
+
+// System path
+define('APPLICATION_DIR', '/var/www/your_project' );
+
+// Google client
+define('APPLICATION_GOOGLE_CLIENT_EMAIL',       '999999-1a1a1a1a1a1a@developer.gserviceaccount.com' );
+define('APPLICATION_GOOGLE_CLIENT_KEY_FILE',    'var/key.p12' );
+define('APPLICATION_GOOGLE_SPREADSHEETS_BOOK',  '' );
+define('APPLICATION_GOOGLE_SPREADSHEETS_SHEET', 'sheet1' );
