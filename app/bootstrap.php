58af1cad95f1f17aae92705f4958e45881faffbd
root <root@training3.training3>
2015-10-23 01:58:38 -0700

    add command line $argv params

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,15 +3,14 @@
 function initialize($basePath)
 {
     error_reporting(E_ALL);
-    ini_set('html_errors','On');
+    ini_set('html_errors','Off');
     ini_set('display_errors','On');
 
     require_once  $basePath . '/app/library/Config.php';
     Config::init( $basePath . '/app/config');
 
     if ( conf('public.base.path') !== $basePath ) {
-       echo 'base path setting error!';
-       echo "\n";
+       show('base path setting error!');
        exit;
     }
 
@@ -30,13 +29,75 @@ function conf($key)
     return Config::get($key);
 }
 
-function pr($data)
-{
-    print_r($data);
-}
-
 function show($data)
 {
-    echo $data;
-    echo "\n";
+    if (is_object($data) || is_array($data)) {
+        print_r($data);
+    }
+    else {
+        echo $data;
+        echo "\n";
+    }
 }
+
+function isCli()
+{
+    return PHP_SAPI === 'cli';
+}
+
+/**
+ *  get command line param or get web param
+ *  
+ *  @dependency isCli()
+ */
+function getParam($key)
+{
+    if (isCli()) {
+        return getCliParam($key);
+    }
+    else {
+        return getWebParam($key);
+    }
+}
+
+function getWebParam($key)
+{
+    if (isset($_POST[$key])) {
+        return $_POST[$key];
+    }
+    elseif (isset($_GET[$key])) {
+        return $_GET[$key];
+    }
+    return null;
+}
+
+/**
+ *  get command line value
+ *
+ *  @return string|int or null
+ */
+function getCliParam($key)
+{
+    global $argv;
+    $allParams = $argv;
+    array_shift($allParams);
+
+    if (in_array($key, $allParams)) {
+        return true;
+    }
+
+    foreach ($allParams as $param) {
+
+        $tmp = explode('=', $param);
+        $name = $tmp[0];
+        array_shift($tmp);
+        $value = join('=', $tmp);
+
+        if ($name===$key) {
+            return $value;
+        }
+    }
+
+    return null;
+}
+
--- public/index.php
+++ public/index.php
@@ -16,11 +16,12 @@ initialize($basePath);
 require_once $basePath . '/app/library/GoogleWorksheetManager.php';
 require_once $basePath . '/app/helper/GoogleApiHelper.php';
 
-
-
-
 perform();
 exit;
+
+// --------------------------------------------------------------------------------
+// 
+// --------------------------------------------------------------------------------
 
 /**
  * 
@@ -31,9 +32,12 @@ function perform()
         show("PHP Version need >= 5.5");
         exit;
     }
-    Log::record('start PHP '. phpversion() );
 
-    //
+    if (!getParam('exec')) {
+        show('---- debug mode ---- (你必須要輸入參數 exec 才會真正執行)');
+    }
+
+    Log::record('start PHP '. phpversion() );
     upgradeGoogleSheet();
 
     Log::record('Done');
@@ -62,8 +66,14 @@ function upgradeGoogleSheet()
     $count = $sheet->getCount();
     for ( $i=0; $i<$count; $i++ ) {
         $row = $sheet->getRow($i);
-        pr($row);
-        // $sheet->setRow($i, $row);
+
+        // 無論如何都必須修改的值
+        $row = filterUnusedCode($row);
+        show($row);
+
+        if (getParam('exec')) {
+            writeSheet($row);
+        }
 
         // debug
         show( $i . ' ' );
@@ -73,3 +83,45 @@ function upgradeGoogleSheet()
     }
 }
 
+/**
+ *  寫入
+ */
+function writeSheet($row)
+{
+    global $sheet;
+
+    show('--------- go go go -------------');
+    return;
+
+    try {
+        $sheet->setRow($i, $row);
+    }
+    catch ( Exception $e) {
+        Log::record( $e->getMessage() );
+        show($e->getMessage());
+        exit;
+    }
+}
+
+/**
+ *  Clean invisible control characters and unused code points
+ *
+ *  \p{C} or \p{Other}: invisible control characters and unused code points.
+ *      \p{Cc} or \p{Control}: an ASCII 0x00–0x1F or Latin-1 0x80–0x9F control character.
+ *      \p{Cf} or \p{Format}: invisible formatting indicator.
+ *      \p{Co} or \p{Private_Use}: any code point reserved for private use.
+ *      \p{Cs} or \p{Surrogate}: one half of a surrogate pair in UTF-16 encoding.
+ *      \p{Cn} or \p{Unassigned}: any code point to which no character has been assigned.
+ *
+ *  該程式可以清除 RIGHT-TO-LEFT MARK (200F)
+ *
+ *  @see http://www.regular-expressions.info/unicode.html
+ *
+ */
+function filterUnusedCode($row)
+{
+    foreach ( $row as $index => $value ) {
+        $row[$index] = preg_replace('/\p{C}+/u', "", $value );
+    }
+    return $row;
+}
