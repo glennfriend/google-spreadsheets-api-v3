91a9930f9d76560ea4a066f3d33bb29c27ead82d
root <root@training3.training3>
2015-10-25 21:46:17 -0700

    update

--- app/helper/MailHelper.php
+++ app/helper/MailHelper.php
@@ -4,23 +4,17 @@ use Nette\Mail\SendmailMailer;
 
 class MailHelper
 {
-    public static function sendStart()
-    {
-        $subject = '[auto] start - '. date("Y-m-d H:i:s");
-        $body    = "";
-        self::send($subject, $body);
-    }
 
     public static function sendSuccess()
     {
-        $subject = '[auto] success - '. date("Y-m-d H:i:s");
+        $subject = '[google-spreadsheets] success - '. date("Y-m-d H:i:s");
         $body    = "";
         self::send($subject, $body);
     }
 
     public static function sendFail()
     {
-        $subject = '[auto] fail - '. date("Y-m-d H:i:s");
+        $subject = '[google-spreadsheets] fail - '. date("Y-m-d H:i:s");
         $body    = "";
         self::send($subject, $body);
     }
@@ -34,7 +28,9 @@ class MailHelper
         $mail = new Message;
         $mail
             ->setFrom('google-spreadsheets-api-v3 <localhost@localhost.com>')
-            ->addTo('your email')
+            // ->addTo('your email')
+            // ->addTo('your email')
+            // ->addTo('your email')
             ->setSubject($subject)
             ->setBody($body);
 
